572c73449b975e53672ee2f1f003000831e3ba89
infinitescaling <shogo564@gmail.com>
2013-12-08 04:33:20 -0500

    used file reader instead, commas were getting removed. time for gui

--- occurence.rb
+++ occurence.rb
@@ -20,56 +20,68 @@ end
 
 
 def start()
-    CSV.foreach(ARGV[0], :quote_char =>"\'") do |row|
-       @parts = row.join(';').split(';')
-       $partscount = @parts
+    File.open(ARGV[0], 'r') do |row|
+      while @line = row.gets
+           @line.chomp!
+           @parts = @line.split(';')
 
-       #unique value
-       if(@parts.length == 2)
-            @parts[1].strip!
+		   #unique value
+		   if(@parts.length == 2)
+				@parts[1].strip!
+				
+				#create hash value if it does not exist
+				$unique[@parts[1]] = 0 if $unique[@parts[1]] == nil
 
-            $unique[@parts[1]] = 0 if $unique[@parts[1]] == nil
-            @count = $unique[@parts[1]]
-            @count = @count+1
-            $unique[@parts[1]] = @count
-            
-            $unique[@parts[1]] = @count
-
-            $total[@parts[1]] = 0 if $total[@parts[1]] == nil
-            @count = $total[@parts[1]]
-            @count = @count+1
-            $total[@parts[1]] = @count
-       
-            #nonunique value
-       elsif(@parts.length > 2)
-           puts @parts
-           @parts.each do | i| 
-        
-                $total[i] = 0 if $total[i] == nil
-                @count = $total[i]
-                @count = @count+1
-                $total[i] = @count
-           end
-       end
+				#increment count for unique hash
+				@count = $unique[@parts[1]]
+				@count = @count+1
+				$unique[@parts[1]] = @count
+				
+				#create hash value if it does not exist
+				$total[@parts[1]] = 0 if $total[@parts[1]] == nil
+				
+				#increment count for total hash
+				@count = $total[@parts[1]]
+				@count = @count+1
+				$total[@parts[1]] = @count
+		   
+				#nonunique value
+		   elsif(@parts.length > 2)
+			   @parts.each do | i| 
+					
+					#create hash value and then increment
+					$total[i] = 0 if $total[i] == nil
+					@count = $total[i]
+					@count = @count+1
+					$total[i] = @count
+			   end
+		   end
+		end
     end
 end
 
 def outputstats
-    $wordList.each_key{|key| 
-
+    $wordList.each do |key|
+        key.strip!
         if($total[key] != "")
-            if($unique[key] == 0)
-                $unique[key] = "0"
-            end
-            puts "#{key}|#{$total[key]|$unique[key]"
+            $unique[key] = 0 if $unique[key] == nil
+            $total[key] =  0 if $total[key] == nil
+            #puts key
+            #puts $unique[key]
+            #puts $total[key]
+            @tempstring = key + "|" + $total[key].to_s +  "|" +  $unique[key].to_s
+            #puts "%s|%s|%s" %s [key,$total[key],$total[key]]
+            puts @tempstring
+            
         else
             puts "#{key}|0|0\n"
         end
-    }
-
+   
+    end
 end
 
 
 
 getWordList()
 start()
+outputstats()
